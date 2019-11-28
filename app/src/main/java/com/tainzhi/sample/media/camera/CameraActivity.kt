7fbbb69b2ba9e328e7750125f5b9feec8dc37256
tainzhi <qfq61@qq.com>
2019-11-28 20:37:19 +0800

    fix: camera ui

--- app/src/main/java/com/tainzhi/sample/media/camera/CameraActivity.kt
+++ app/src/main/java/com/tainzhi/sample/media/camera/CameraActivity.kt
@@ -1,6 +1,8 @@
 package com.tainzhi.sample.media.camera
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.tainzhi.sample.media.R
 
@@ -14,7 +16,13 @@ import com.tainzhi.sample.media.R
 class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_camera)
+
         savedInstanceState ?: supportFragmentManager.beginTransaction()
                 .replace(R.id.container, Camera2BasicFragment.newInstance())
                 .commit()
