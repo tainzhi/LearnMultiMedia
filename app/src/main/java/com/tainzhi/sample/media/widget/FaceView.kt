3cfcd4b7a81ba2646d9b0a657ff48e8ca77164d5
tainzhi <qfq61@qq.com>
2020-08-22 23:04:51 +0800

    add faceview

---
+++ app/src/main/java/com/tainzhi/sample/media/widget/FaceView.kt
@@ -0,0 +1,49 @@
+package com.tainzhi.sample.media.com.tainzhi.sample.media.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+import com.tainzhi.sample.media.com.tainzhi.sample.media.util.dp
+
+/**
+ * @author:      tainzhi
+ * @mail:        qfq61@qq.com
+ * @date:        2020/8/22 22:57
+ * @description:
+ **/
+
+class FaceView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private var paint: Paint
+    
+    // 人脸识别框颜色
+    private val color = "#42ed45"
+    var faces: ArrayList<RectF>? = null
+        set(value) {
+            field = value
+            invalidate()
+        }
+    
+    init {
+        paint = Paint().apply {
+            this.color = Color.parseColor(this@FaceView.color)
+            style = Paint.Style.STROKE
+            strokeWidth = 1.dp()
+            isAntiAlias = true
+        }
+    }
+    
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        faces?.let {
+            it.forEach {
+                canvas.drawRect(it, paint)
+            }
+        }
+    }
+}
